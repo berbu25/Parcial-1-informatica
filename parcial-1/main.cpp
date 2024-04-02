c4050e91eb60f5ec6a3c9e25f5d49bdb3f31f2cc
SalomeBM <125213769+SalomeBM@users.noreply.github.com>
2024-04-02 00:10:09 -0500

    Integración de la función rotar matriz

--- parcial-1/main.cpp
+++ parcial-1/main.cpp
@@ -2,6 +2,61 @@
 
 using namespace std;
 
+void rotar_matriz(int **matriz, int m, int opcion) {
+    switch (opcion) {
+    case 1:
+        // Rotar 90 grados a la derecha
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = i; j < m - i - 1; j++) {
+                // Intercambiar elementos
+                int temp = matriz[i][j];
+                matriz[i][j] = matriz[m - 1 - j][i];
+                matriz[m - 1 - j][i] = matriz[m - 1 - i][m - 1 - j];
+                matriz[m - 1 - i][m - 1 - j] = matriz[j][m - 1 - i];
+                matriz[j][m - 1 - i] = temp;
+            }
+        }
+        break;
+    case 2:
+        // Rotar 180 grados
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = 0; j < m; j++) {
+                // Intercambiar elementos
+                int temp = matriz[i][j];
+                matriz[i][j] = matriz[m - 1 - i][m - 1 - j];
+                matriz[m - 1 - i][m - 1 - j] = temp;
+            }
+        }
+        break;
+    case 3:
+        // Rotar 90 grados a la izquierda
+        for (int i = 0; i < m / 2; i++) {
+            for (int j = i; j < m - i - 1; j++) {
+                // Intercambiar elementos
+                int temp = matriz[i][j];
+                matriz[i][j] = matriz[j][m - 1 - i];
+                matriz[j][m - 1 - i] = matriz[m - 1 - i][m - 1 - j];
+                matriz[m - 1 - i][m - 1 - j] = matriz[m - 1 - j][i];
+                matriz[m - 1 - j][i] = temp;
+            }
+        }
+        break;
+    default:
+        cout << "Opción no válida." << endl;
+        break;
+    }
+
+    // Imprimir la matriz rotada
+    cout << "Matriz rotada:" << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main() {
     // Dimensión de la matriz
     int m;
